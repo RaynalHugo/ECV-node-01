0072d7a008961a6d15c09b016d49a4047618e3bf
Lucas-Debiais <lucas.debiais11@gmail.com>
2022-11-10 09:22:22 +0100

    Feat: ++version

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ async function getPackageJsonVersion() {
   const data = await fs.readFile("./package.json");
   const string = data.toString();
   const json = JSON.parse(string);
+
+  let version = json.version.split('.');
+  ++version[2];
+  version = version.join('.');
+  json.version = version;
+
+  const newString = JSON.stringify(json, null, 2);
+  await fs.writeFile("./package.json", newString);
+
   return json.version;
 }
 
 const version = await getPackageJsonVersion();
-console.log("version:", version);
-console.log("hello");
